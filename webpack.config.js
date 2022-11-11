5f7cf3c7697c9847185413d0e99bb00387fab060
autumnly1007 <autumnly1007@gmail.com>
2022-11-11 21:51:58 +0900

    Add webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    compress: true,
+    port: 9999,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: 'setup webpack & babel',
+      template: 'index.html',
+    }),
+  ],
+};
